043973c6e4ca8f307d23b7690a976422a1594585
IW <122602491+FadeIgor@users.noreply.github.com>
2025-11-04 15:44:43 +0100

    Add new SELECT query with ordering for clients

--- cos.sql
+++ cos.sql
@@ -7,4 +7,16 @@ FROM klienci
 	RIGHT JOIN wypozyczenia
     	on klienci.id_klienta=wypozyczenia.id_klienta;
 
+2
 
+SELECT 
+	klienci.imie_klienta,
+    klienci.nazwisko_klienta,
+    wypozyczenia.id_wypozyczenia,
+    wypozyczenia.data_wyp
+FROM klienci RIGHT JOIN wypozyczenia
+    	on klienci.id_klienta=wypozyczenia.id_klienta  
+ORDER BY `klienci`.`nazwisko_klienta`  ASC
+
+
+3
